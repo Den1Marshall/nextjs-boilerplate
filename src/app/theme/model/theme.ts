663b78e51ba5c92bcf9d432963219c4590a82bfb
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-03-05 00:29:02 +0200

    fix: add fallback for roboto font

--- src/app/theme/model/theme.ts
+++ src/app/theme/model/theme.ts
@@ -4,6 +4,7 @@ import { Roboto } from 'next/font/google';
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '900'],
+  fallback: ['system-ui', 'arial'],
 });
 
 export const theme = extendTheme({
