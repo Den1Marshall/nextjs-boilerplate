c6a4ec7bd17cd9939bf63783337d91a23404ee40
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-04-20 17:58:58 +0300

    feat: migrate from next-pwa to @ducanh2912/next-pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
 });
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
