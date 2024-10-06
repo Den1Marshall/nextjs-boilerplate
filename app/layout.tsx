08370c14db04b1dc8790315f44a310f1fe552dd7
Denys Hrychulevych <111864961+Den1Marshall@users.noreply.github.com>
2024-10-06 03:37:25 +0300

    feat: add google analytics via @next/third-parties
    Disabled by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,7 @@ export default function RootLayout({
           <AriaRouterProvider>{children}</AriaRouterProvider>
         </FramerMotionConfig>
       </body>
+      {/* <GoogleAnalytics gaId="G-XYZ" /> */}
     </html>
   );
 }
